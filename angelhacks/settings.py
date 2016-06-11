743243996888a0b007efb92d6b5c2009f728d712
david <david@largitdata.com>
2016-06-11 14:23:12 +0800

    add local change

--- angelhacks/settings.py
+++ angelhacks/settings.py
@@ -82,4 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/webapps/angelhacks/static/'
+#STATIC_ROOT = '/webapps/angelhacks/static/'
+STATICFILES_DIRS = (
+    '/webapps/angelhacks/static/',
+)
