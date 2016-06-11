6b7c6460c0c1db38161a6ba1472386f6e3a34845
Ubuntu <tr.ywchiu@gmail.com>
2016-06-11 07:05:57 +0000

    addd local change

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 import rethinkdb as r
 import json
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger('django')
+
+def store_message(event):
+    data = {}
+    data['userid'] = event['sender']['id']
+    data['content'] = event['message']['text']
+    conn = r.connect( "localhost", 28015).repl()
+    feed = r.db('angelhacksdb').table("userfeed").insert(data).run(conn)
+    conn.close()
+    
 
 def index(request):
     context = {'latest_question_list': 'latest_question_list'}
@@ -18,8 +33,25 @@ def getMessage(request):
     return HttpResponse(json.dumps(context), content_type="application/json")
     #context = {'latest_question_list': 'latest_question_list'}
 
+@csrf_exempt
 def webhook(request):
     if request.method == 'POST':
-        data = request.body
-        logger.debug(data)
+        data = {}
+        msg = json.loads(request.body.decode('utf-8'))
+        if msg['object'] == 'page':
+            for pageEntry in msg['entry']:
+                data['timestamp'] = pageEntry['time']
+                for event in pageEntry['messaging']:
+                    if 'delivery' in event:
+                        logger.debug("DELIVERY A MESSAGE {}".format(event['delivery']))
+                    elif 'message' in event:
+                        logger.debug("RECEIVED A MESSAGE {}".format(event['message']['text'].encode('utf-8')))
+                        store_message(event)
+                    elif 'postback' in event:
+                        logger.debug("RECEIVED A POSTBACK {}".format(event['postback']))
+                    else:
+                        logger.debug("RECEIVED AN EVENT {}".format(event))
+
+    context = {'hello': 'ok'}
+    return HttpResponse(json.dumps(context), content_type="application/json")
 
